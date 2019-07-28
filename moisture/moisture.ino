d32980623555db72ae38779949d65d0b97d0d34a
JOPHIN1992 <43670924+JOPHIN1992@users.noreply.github.com>
2019-07-28 19:06:47 +0530

    Add files via upload

---
+++ moisture/moisture.ino
@@ -0,0 +1,17 @@
+  float b,a;
+ // int a,b;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(A0,INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+b=analogRead(A0);
+a=(100-((b/1023)*100));
+Serial.println("moisture value=");
+Serial.println(a);
+delay(1000);
+}
